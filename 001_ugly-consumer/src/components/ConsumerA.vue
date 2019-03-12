f2d3bf7303e4395f9cf2de1cd13c08fce25ea412
Allan <allanchua.officefiles@gmail.com>
2019-03-12 22:03:33 +0800

    Created demo for not so good api consumption

---
+++ 001_ugly-consumer/src/components/ConsumerA.vue
@@ -0,0 +1,44 @@
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  color: #640f0f;
+  display: inline-block;
+  margin: 32px 10px;
+}
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "ConsumerA",
+  async mounted() {
+    let { data } = await axios.get("http://localhost:3000/users");
+
+    this.users = data.users;
+  },
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      users: []
+    };
+  }
+};
+</script>
+
+<template>
+    <ul>
+      <h3>User List A</h3>
+      <li v-for="user in users"
+        :key="user.id">{{user.first_name}} {{user.last_name}}</li>
+    </ul>
+</template>
